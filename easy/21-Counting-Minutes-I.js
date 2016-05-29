2e15285e61bfdc014d91c2e48806ad050698a79e
Victor Lin <vlin@Victors-MacBook-Pro.local>
2016-05-29 09:34:30 -0500

    easy 23

--- easy/21-Counting-Minutes-I.js
+++ easy/21-Counting-Minutes-I.js
@@ -6,7 +6,33 @@
 // If str is 1:00pm-11:00am the output should be 1320.
 
 function CountingMinutesI(str) {
+  //str
+  //00:00pm-00:00pm
+  //0:00pm-00:00pm
+  //00:00pm-0:00pm
+  //0:00pm-0:00pm
+  str=str.split("")
+  if(str.length<15 && str[5]==='m'){
+    str.unshift('0')
+  }
+  if (str.length<15 && str[str.length-1]==='m'){
+    str.splice(8,0,'0')
+  }
+  str.pop()
+  str=str.join("").replace("m","").split("-")
 
+
+  var Count=function(time1,time2){
+    count=0
+    time1=time1.split('');
+    time2=time2.split('');
+    time1.unshift(time1.pop());
+    time2.unshift(time2.pop());
+
+    return count
+  }
+
+  Count(str[0],str[1])
   // code goes here
   return str;
 
